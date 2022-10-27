cd70cafed86c251ebaae4913d36891fd6fb9285f
shirwac62 <c.naafacaxmed962@gmail.com>
2022-10-27 17:38:03 +0300

    Updating 2022-10-27

--- custom_adons/om_hospital/models/appointment.py
+++ custom_adons/om_hospital/models/appointment.py
@@ -5,8 +5,35 @@ class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = "Hospital Appointment"
+    _rec_name = 'patient_id'
 
     patient_id = fields.Many2one('hospital.patient', string='Patient')
     gender = fields.Selection(related='patient_id.gender')
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)
+    ref = fields.Char(string='Reference')
+    prescription = fields.Html(string='Prescription')
+    priority = fields.Selection([
+        ('0', 'Normal'),
+        ('1', 'Low'),
+        ('2', 'High'),
+        ('3', 'Very High')], string='Priority')
+    status = fields.Selection([
+        ('draft', 'Draft'),
+        ('in_consultation', 'In Consultation'),
+        ('done', 'Done'),
+        ('cancel', 'Cancelled')], default="draft", string='Status', required=True)
+
+    @api.onchange('patient_id')
+    def onchange_patient_id(self):
+        self.ref = self.patient_id.ref
+
+    def object_button(self):
+        print("Button Clicked")
+        return {
+            'effect': {
+                'fadeout': 'slow',
+                'message': 'click successful',
+                'type': 'rainbow_man'
+            }
+        }
--- custom_adons/om_hospital/models/patient.py
+++ custom_adons/om_hospital/models/patient.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from odoo import api, models, fields
 
 
@@ -8,6 +10,16 @@ class HospitalPatient(models.Model):
 
     name = fields.Char(string='Name', tracking=True)
     ref = fields.Char(string='Reference')
-    age = fields.Integer(string='Age', tracking=True)
+    date_of_birth = fields.Date(string='Date Of  Birth')
+    age = fields.Integer(string='Age', compute='_compute_age', tracking=True)
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender', tracking=True)
     active = fields.Boolean(string="Active", default=True)
+
+    @api.depends('date_of_birth')
+    def _compute_age(self):
+        for rec in self:
+            today = date.today()
+            if rec.date_of_birth:
+                rec.age = today.year - rec.date_of_birth.year
+            else:
+                rec.age = 0
